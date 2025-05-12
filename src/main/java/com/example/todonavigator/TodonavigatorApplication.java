082ebc1e91d5feb0f0804a406aa5500bed83b660
DELL <DELL@DESKTOP-RA8R57A>
2025-05-12 11:47:53 +0530

    first commit

---
+++ src/main/java/com/example/todonavigator/TodonavigatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.todonavigator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodonavigatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodonavigatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todonavigator/TodonavigatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todonavigator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodonavigatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
